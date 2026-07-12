1012fa636f93d9ea1a15f1e264be8884e7a9d5d5
Lai Hoe Xun <hoexunlai@gmail.com>
2026-07-12 14:23:47 +0800

    Add: string-encode-and-decode - submission-10

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-10.py
@@ -0,0 +1,20 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        res = ""
+        for s in strs:
+            res+=str(len(s))+"#"+s
+        return res
+    def decode(self, s: str) -> List[str]:
+
+        res,i = [],0
+        while i < len(s):
+            j = i
+            while s[j]!="#":
+                j +=1
+            length = int(s[i:j]) #this is the integer
+
+            res.append(s[j+1:j+1+length]) #index j+1 is at the 1st letter of the string, append all the letters till end of the string
+            i=j+1+length #this is the index of the new string
+        return res
+
